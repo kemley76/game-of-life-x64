6ff9406c0f4f601bdb10888c931ef73bac2764a9
kemley76 <kaden.emley@gmail.com>
2025-01-19 16:45:57 -0500

    swapping checkerboard
    Signed-off-by: kemley76 <kaden.emley@gmail.com>

--- gol.s
+++ gol.s
@@ -1,25 +1,72 @@
-SIZE equ 20
+SIZE equ 31
 
 global _start 
 section .text
 _start:
+	; calculate grid size into r12
 	mov rax, SIZE
-	mov r14, SIZE
-	mul r14
+	mov r12, SIZE
+	mul r12
 	mov r12, rax ; r12 = number of bytes in the grid
-	mov r8, r12 ; r8 = counter (from size^2 to 0)
 
 	mov r13, rsp ; r13 holds starting address to grid
-	mov rax, r13
 	sub rsp, r12 ; make space for grid on stack
-	initialize: ; loop through every row
-		mov byte [rax], 1
-		mov byte [rax + 1], 0
-		add rax, 2
-		sub r8, 2
-		cmp r8, 0
-		jg initialize 
 
+	mov r14, rsp ; r14 holds starting address to grid 2
+	sub rsp, r12 ; make space for grid on stack
+
+	mov r8, 0 ; loop counter
+
+	initialize: ; loop through every row
+		mov byte [r13 + r8], 1
+		mov byte [r14 + r8], 0
+		inc r8
+		mov byte [r13 + r8], 0
+		mov byte [r14 + r8], 1
+		;add rax, 2
+		inc r8
+		cmp r8, r12
+		jle initialize 
+
+	loop: 
+		call printGrid
+		xchg r13, r14
+
+		mov rsi, nl; buf = '\n'
+		call print
+
+		call sleep
+		call printGrid
+		call sleep
+		jmp loop
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+print: ; put string buffer into rsi
+	mov rax, 1; print
+	mov rdi, 0; fd = stdout
+	mov rdx, 1; count = 1
+	syscall
+	ret
+
+print2: 
+	mov rax, 1
+	mov rdi, 0
+	mov rsi, cursor
+	mov rdx, len
+
+	syscall
+	ret
+
+printGrid: ; print grid starting starting at r13
+	push rbp
+	mov rbp, rsp
+
+	push r14
+
+	call print2
 	mov r8, 0
 	mov r14b, 0
 	printLoop:
@@ -46,21 +93,34 @@ _start:
 	mov rsi, nl; buf = xStr
 	call print
 
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+	pop r14 
 
-print: 
-	mov rax, 1; print
-	mov rdi, 0; fd = stdout
-	mov rdx, 1; count = 1
+	mov rsp, rbp
+	pop rbp
+	ret
+
+sleep: 
+	mov rax, 35
+	mov rdi, sleep_time
+	mov rsi, 0
 	syscall
 	ret
 
+%macro timespec 2
+	dq %1
+	dq %2
+%endmacro
+
 section .data
+
+	sleep_time timespec 0, 500000000
+
 	message: db 0xA; 0xA == \n
 	message_length equ $-message
 
 	oStr: db "O"
 	xStr: db "X"
 	nl: db 0x0A
+
+	cursor db 0x1B, '[', 'H' ; escape sequence to go to home position
+	len equ $-cursor
